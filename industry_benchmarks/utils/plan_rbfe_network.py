54daf7394a0a2f59806e2c4dca73a983bca9c061
PabloNA97 <p.navarro.acero@gmail.com>
2024-12-03 15:25:32 +0100

    increase default repeats

--- industry_benchmarks/utils/plan_rbfe_network.py
+++ industry_benchmarks/utils/plan_rbfe_network.py
@@ -121,7 +121,7 @@ def get_settings():
     # Should we use this new OpenFF version or the default?
     settings.forcefield_settings.small_molecule_forcefield = 'openff-2.2.0'
     # Only run one repeat per input json file
-    settings.protocol_repeats = 1
+    settings.protocol_repeats = 3
     return settings
 
 
@@ -142,7 +142,7 @@ def get_settings_charge_changes():
     settings.simulation_settings.n_replicas = 22
     settings.lambda_settings.lambda_windows = 22
     # Only run one repeat per input json file
-    settings.protocol_repeats = 1
+    settings.protocol_repeats = 3
     return settings
 
 
